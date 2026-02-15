b7e77827803c92daa040376b91a1177383ad4aeb
ShubhamNayk <rajranjeet862010@gmail.com>
2026-02-15 12:30:50 +0530

    updated firebase env config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDydyC8b8-tNQnZuFwqYpQ7oNuBzvXWzqQ",
-  authDomain: "agis-protocol.firebaseapp.com",
-  projectId: "agis-protocol",
-  storageBucket: "agis-protocol.firebasestorage.app",
-  messagingSenderId: "265683493278",
-  appId: "1:265683493278:web:44d7626daf7404e0cbb68f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
